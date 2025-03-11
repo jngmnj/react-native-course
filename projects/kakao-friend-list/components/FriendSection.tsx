1a1eda36cb3b6363c5d2e1d250d3a867f3c1b8bd
jngmnj <exorbitance@naver.com>
2025-03-11 16:34:39 +0900

    feat: 친구목록상단 토글버튼과 친구카운트 컴포넌트생성

---
+++ projects/kakao-friend-list/components/FriendSection.tsx
@@ -0,0 +1,28 @@
+import { MaterialIcons } from '@expo/vector-icons';
+import React from 'react';
+import { Text, TouchableOpacity, View } from "react-native";
+
+interface FriendSectionProps {
+  friendProfileLen: number;
+  onPress: () => void;
+  isOpen: boolean;
+}
+const FriendSection = (props: FriendSectionProps) => {
+  return (
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+        paddingHorizontal: 20,
+      }}
+    >
+      <Text style={{ color: "grey" }}>친구 {props.friendProfileLen}</Text>
+      <TouchableOpacity onPress={props.onPress}>
+        <MaterialIcons name={props.isOpen ? "keyboard-arrow-up" : "keyboard-arrow-down"} size={24} color="grey" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default FriendSection
