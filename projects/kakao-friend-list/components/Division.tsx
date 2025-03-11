08b43bd6a957f347e589f44ebac32dbe351f2573
jngmnj <exorbitance@naver.com>
2025-03-11 16:33:07 +0900

    feat: 구분선 컴포넌트 추가

---
+++ projects/kakao-friend-list/components/Division.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Division = () => {
+  return (
+    <View style={{ height: 0.5, backgroundColor: 'lightgrey', width: "100%" }} />    
+  )
+}
+
+export default Division
