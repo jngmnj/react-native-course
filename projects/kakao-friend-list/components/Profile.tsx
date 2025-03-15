f48189f1e5684d9aea1a77df07f3fd316ecec330
jngmnj <exorbitance@naver.com>
2025-03-15 22:12:05 +0900

    refactor: prop 수정, 불필요한 View, StyleSheet 삭제&수정

--- projects/kakao-friend-list/components/Profile.tsx
+++ projects/kakao-friend-list/components/Profile.tsx
@@ -1,61 +1,25 @@
 import { ProfileType } from "@/assets/data";
 import React from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
+import { Image, Text, View } from "react-native";
 
 interface MyProfileProps extends ProfileType {
-  size?: "small" | "large";
+  isMe: boolean;
 }
-export default (props: MyProfileProps) => {
+export default ({ isMe, source, name, introduction }: MyProfileProps) => {
+  const size = isMe ? 50 : 40;
 
   return (
-    <View style={styles.ProfileContainer}>
-      <View
-        style={
-          props.size == "small" ? styles.smallImageArea : styles.largeImageArea
-        }
-      >
-        {/* require(props.source): 동적매핑이 안된다고함 */}
-        {/* images[props.source] */}
-        <Image source={props.source} style={{ width: 50, height: 50 }} />
-      </View>
-      <View style={styles.textArea}>
-        <Text
-          style={{
-            fontWeight: "bold",
-            marginBottom: 4,
-            fontSize: props.size == "small" ? 14 : 16,
-          }}
-        >
-          {props.name}
-        </Text>
-        <Text style={{ fontSize: 12, color: "gray" }}>
-          {props.introduction}
-        </Text>
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <Image source={source} style={{ width: size, height: size, borderRadius: size * 0.4 }} />
+      <View style={{ justifyContent: "center", marginLeft: 12 }}>
+        <Text style={{fontWeight: isMe ? 700 : 500, fontSize: isMe ? 16 : 15,}}>{name}</Text>
+        {!!introduction && ( // introduction이 존재할 때만 렌더링(falsy값이면 렌더링 안함)
+          <View>
+            <View style={{ height: isMe ? 6 : 2 }} />
+            <Text style={{ fontSize: isMe ? 12 : 11, color: "gray" }}>{introduction}</Text>
+          </View>
+        )}
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  ProfileContainer: {
-    flexDirection: "row",
-    alignItems: "center",
-    width: "100%",
-    // paddingVertical: 10,
-  },
-  largeImageArea: {
-    width: 50,
-    height: 50,
-    borderRadius: 20,
-    overflow: "hidden",
-  },
-  smallImageArea: {
-    width: 40,
-    height: 40,
-    borderRadius: 16,
-    overflow: "hidden",
-  },
-  textArea: {
-    marginLeft: 12,
-  },
-});
