fc55773bd0fa45dc6a2177f4e4636eeb1a660daa
jngmnj <exorbitance@naver.com>
2025-03-15 22:10:44 +0900

    refactor: ScrollView를 FlatList로 변경, 불필요한 StyleSheet삭제, Margin 추가

--- projects/kakao-friend-list/app/(tabs)/index.tsx
+++ projects/kakao-friend-list/app/(tabs)/index.tsx
@@ -1,18 +1,17 @@
 import { friendProfiles, myProfile } from "@/assets/data";
-import FriendList from "@/components/FriendList";
+import Division from "@/components/Division";
 import FriendSection from "@/components/FriendSection";
-import Header from '@/components/Header';
+import Header from "@/components/Header";
 import Margin from "@/components/Margin";
 import Profile from "@/components/Profile";
 import { useState } from "react";
-import { StyleSheet, View } from "react-native";
-import Division from '../../components/Division';
+import { FlatList, View } from "react-native";
 
 export default function HomeScreen() {
   const [isFriendSectionOpen, setIsFriendSectionOpen] = useState(true);
 
-  return (
-    <View style={styles.container}>
+  const ListHeaderComponent = () => (
+    <View style={{ backgroundColor: "#fff" }}>
       <Header />
 
       {/* 컴포넌트별 간격 지정시 명시적으로 height주는게 좋음 */}
@@ -22,6 +21,7 @@ export default function HomeScreen() {
         source={myProfile.source}
         name={myProfile.name}
         introduction={myProfile.introduction}
+        isMe={true}
       />
 
       <Margin height={10} />
@@ -33,18 +33,33 @@ export default function HomeScreen() {
         onPress={() => setIsFriendSectionOpen(!isFriendSectionOpen)}
         isOpen={isFriendSectionOpen}
       />
-      {/* isOpen이 true일 때만 FriendList 컴포넌트가 보이도록 설정 */}
-      {isFriendSectionOpen && <FriendList friendProfiles={friendProfiles} />}
+      <Margin height={5} />
+    </View>
+  );
+  const ListFooterComponent = () => <Margin height={10} />;
+  const ItemSeparatorComponent = () => <Margin height={10} />;
+  const renderItem = ({ item }: any) => (
+    <View>
+      <Profile
+        name={item.name}
+        source={item.source}
+        introduction={item.introduction}
+        isMe={false}
+      />
+    </View>
+  );
+  return (
+    <View style={{ paddingHorizontal: 20 }}>
+      <FlatList
+        data={isFriendSectionOpen ? friendProfiles : []}
+        keyExtractor={(_, index) => index.toString()}
+        ItemSeparatorComponent={ItemSeparatorComponent}
+        renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
+        ListFooterComponent={ListFooterComponent}
+        stickyHeaderIndices={[0]} // ListHeaderComponent를 고정시키는 속성
+        showsVerticalScrollIndicator={false}
+      ></FlatList>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    gap: 8,
-    borderStyle: "solid",
-    height: "100%",
-    paddingHorizontal: 20,
-  },
-});
